c6a42c18661f3b966e1e944a75ed074d6b35c780
Prem-Semitha <premsemitha@gmail.com>
2023-10-18 15:31:30 +0530

    add double linked list

--- linkedList/doubleLinkedList/insertion.cpp
+++ linkedList/doubleLinkedList/insertion.cpp
@@ -40,12 +40,40 @@ void display(Node * head){
     }
     cout<<endl;
 }
+
+void deleteAtHead(Node *head){
+  Node *toDelete=head;
+  head=head->next;
+  head->prev=NULL;
+}
+
+void deletion(Node *head,int pos){
+  Node *temp=head;
+  int count=1;
+
+  if(pos==1){
+    deleteAtHead(head);
+    return;
+  }
+  while(count != pos && temp!=NULL){ 
+    temp=temp->next;
+    count++;
+  }
+  temp->prev->next=temp->next;
+  if(temp->next != NULL){
+  temp->next->prev=temp->prev;
+  }
+  delete temp; 
+}
+
 int main(){
   Node *head=NULL;
   insertAtTail(head,5);
   insertAtTail(head,4);
   insertAtTail(head,8);
   insertAtTail(head,7);
+  display(head);
+  deletion(head,2);
   display(head);
   return 0;
 }
